80f5882232e77c5fde9ae617f42a4cdf9eaba518
정다은 <ekdms8652@pusan.ac.kr>
2025-04-01 01:32:13 +0900

    다항식: 단순 오류 수정

--- polynomail1.c
+++ polynomail1.c
@@ -19,7 +19,7 @@ polynomial poly_add1(polynomial A, polynomial B){
             C.coef[Cpos++] = A.coef[Apos++];
             degree_a--;
         }
-        else if(degree_a = degree_b){
+        else if(degree_a == degree_b){
             C.coef[Cpos++] = A.coef[Apos++] + B.coef[Bpos++];
             degree_a--; degree_b--;
         }
@@ -33,17 +33,17 @@ polynomial poly_add1(polynomial A, polynomial B){
 
 void print_poly(polynomial p){
     for(int i = p.degree; i > 0; i--)
-        printf("%3.1fx^d + ", p.coef[p.degree-i], i);
+        printf("%3.1fx^%d + ", p.coef[p.degree-i], i);
     printf("%3.1f \n", p.coef[p.degree]);
 }
 int main(){
-    polynomial a = {5,{3,6,0,0,10}};
+    polynomial a = {5,{3,6,0,0,0,10}};
     polynomial b = {4, {7,0,5,0,1}};
     polynomial c;
     print_poly(a);
     print_poly(b);
     c = poly_add1(a, b);
-    printf("------------------------");
+    printf("------------------------\n");
     print_poly(c);
     return 0;
 }
