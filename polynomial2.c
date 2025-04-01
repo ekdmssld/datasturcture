6a0ceb0ce28d299f982db049f875778a91abd9b1
정다은 <jeongda-eun@jeongda-eun-ui-MacBookAir.local>
2025-04-01 20:54:46 +0900

    다항식 표현방법2

--- polynomial2.c
+++ polynomial2.c
@@ -7,13 +7,13 @@ typedef struct{
     int expon;
 }polynomial;
 
-polynomial terms[MAX_TERM] = {{8,3}, {7,1}, { 1,0} ,{10,3},{10,3},{3,2},{1,0}};
-int avail = 0;
+polynomial terms[MAX_TERM] = {{8,3}, {7,1}, { 1,0} ,{10,3},{3,2},{1,0}};
+int avail = 6;
 
 char compare(int a, int b){
-    if(a>b) return ">";
-    else if(a==b) return "=";
-    else return "<";
+    if(a>b) return '>';
+    else if(a==b) return '=';
+    else return '<';
 }
 
 void attach(float coef, int expon){
@@ -55,6 +55,7 @@ void poly_add2(int As, int Ae, int Bs, int Be, int *Cs, int *Ce){
 void print_poly(int s, int e){
     for(int i = s; i < e; i++)
         printf("%3.1fx^%d + ", terms[i].coef, terms[i].expon);
+    printf("%3.1fx^%d \n", terms[e].coef, terms[e].expon);
 }
 int main(){
     int As = 0, Ae=2, Bs=3, Be=5, Cs, Ce;
