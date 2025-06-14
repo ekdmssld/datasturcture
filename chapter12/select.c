4dd3f9ce2212a6a3052fd2d7c2c547d17fbf4e64
ekdmssld <ekdms8652@gmail.com>
2025-06-14 21:57:30 +0900

    버블 정렬

--- chapter12/select.c
+++ chapter12/select.c
@@ -27,6 +27,16 @@ void insertion_sort(int list[], int n){
         list[j+1] = key;
     }
 }
+
+void bubble_sort(int list[], int n){
+    int i, j, temp;
+    for(i = n-1; i > 0; i--){
+        for(j = 0; j < i; j++){
+            if(list[j] > list[j+1])
+                SWAP(list[j], list[j+1], temp);
+        }
+    }
+}
 int main(){
     int i;
     n = MAX;
