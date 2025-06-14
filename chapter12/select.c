ee09e9db5b6c4c646e16b168ef8e165bc9ba29c0
ekdmssld <ekdms8652@gmail.com>
2025-06-14 22:04:48 +0900

    쉘 정렬

--- chapter12/select.c
+++ chapter12/select.c
@@ -37,6 +37,24 @@ void bubble_sort(int list[], int n){
         }
     }
 }
+
+void inc_insert_sort(int list[], int first, int last, int gap){
+    int i, j, key;
+    for(i = first+gap; i <= last; i += gap){
+        key = list[i];
+        for(j = i-gap; j >= first && list[j] > key; j -= gap)
+            list[j+gap] = list[j];
+        list[j+gap] = key;
+    }
+}
+
+void shell_sort(int list[], int n){
+    int gap, i;
+    for(gap = n/2; gap > 0; gap /= 2){
+        for(i = 0; i < gap; i++)
+            inc_insert_sort(list, i, n-1, gap);
+    }
+}
 int main(){
     int i;
     n = MAX;
