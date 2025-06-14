e9d0469603e0dc617f47f5979c175a7f30843ccc
ekdmssld <ekdms8652@gmail.com>
2025-06-14 22:12:39 +0900

    합병 정렬

--- chapter12/select.c
+++ chapter12/select.c
@@ -55,6 +55,27 @@ void shell_sort(int list[], int n){
             inc_insert_sort(list, i, n-1, gap);
     }
 }
+int sorted[MAX];
+void merge(int list[], int left, int mid, int right){
+    int i, j, k, l;
+    i = left; j = mid + 1; k = left;
+    while(i <= mid && j <= right){
+        if(list[i] <= list[j])
+            sorted[k++] = list[i++];
+        else
+            sorted[k++] = list[j++];
+    }
+    if(i > mid){
+        for(l = j; l <= right; l++)
+            sorted[k++] = list[l];
+    }
+    else{
+        for(l = i; l <= mid; l++)
+            sorted[k++] = list[l];
+    }
+    for(l = left; l <= right; l++)
+        list[l] = sorted[l];
+}
 int main(){
     int i;
     n = MAX;
